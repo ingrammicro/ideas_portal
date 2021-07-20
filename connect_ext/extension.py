3b5306e7b8855526008e00a9f625c50f4408e44f
Agostina Kodelia <agostina.kodelia@cloudblue.com>
2021-07-20 10:41:53 +0200

    LITE-19577: Implement action product, for ideas portal sso

--- connect_ext/extension.py
+++ connect_ext/extension.py
@@ -3,6 +3,8 @@
 # Copyright (c) 2021, Globex Corporation
 # All rights reserved.
 #
+import time
+from jwt import encode, decode
 
 from connect.eaas.extension import (
     Extension,
@@ -13,6 +15,21 @@ from connect.eaas.extension import (
 
 class IdeasPortalExtension(Extension):
     
+    def _calculate_aha_token(self, request):     
+        data = request['form_data']
+        connect_token = request['querystring']['jwt'][0]
+        jwt_decoded = decode(connect_token, self.config['CONNECT_JWT_SECRET'],  algorithms='HS256')
+
+        payload = {}
+        payload['iat'] = int(time.time())
+        payload['jti'] = jwt_decoded['asset_id']
+        payload['first_name'] = data['givenName']
+        payload['last_name'] = data['familyName']
+        payload['email'] = data['email']
+        payload['exp'] = time.time() + int(self.config.get("TOKEN_EXP_MINUTES", 1)) * 60
+
+        return encode(payload, self.config['AHA_JWT_SECRET'])
+
     def process_asset_purchase_request(self, request):
         request_id = request['id']
         self.logger.info(f"Obtained request with id {request_id}")
@@ -29,4 +46,16 @@ class IdeasPortalExtension(Extension):
     
     def execute_product_action(self, request):
         self.logger.info(f'Product action: {request}')
-        return ProductActionResponse.done()
+
+        if request['method'] == 'GET':
+            return ProductActionResponse.done(
+                http_status=302,
+                headers={'Location': self.config['DEFAULT_REDIRECT']}
+            )
+
+        aha_token = self._calculate_aha_token(request)
+        aha_login_url = self.config['AHA_LOGIN_URL']
+        return ProductActionResponse.done(
+            http_status=302,
+            headers={'Location': f"{aha_login_url}?jwt={aha_token}"}
+        )
--- tests/test_ideasportal.py
+++ tests/test_ideasportal.py
@@ -4,6 +4,7 @@
 # All rights reserved.
 #
 
+import re
 import pytest
 
 
@@ -43,3 +44,35 @@ def test_process_asset_purchase_request_raise_error(
     with pytest.raises(connect.client.exceptions.ClientError) as excinfo:
         ext.process_asset_purchase_request(request)
         assert '400 Bad Request' in excinfo.value
+
+
+def test_process_product_action(
+    logger,
+    product_action_request_factory,
+    product_action_config_factory
+):
+    config = product_action_config_factory()
+
+    aha_login_url = config['AHA_LOGIN_URL']
+    request = product_action_request_factory('POST', config['CONNECT_JWT_SECRET'])
+    ext = IdeasPortalExtension(None, logger, config)
+    
+    result = ext.execute_product_action(request)
+    assert result.status == 'success'
+    assert result.http_status == 302
+    assert re.match(re.compile(f'{aha_login_url}\\?jwt=.+'), result.headers['Location'])
+
+
+def test_process_product_action_get_method(
+    logger,
+    product_action_config_factory,
+    product_action_request_factory
+):
+    config = product_action_config_factory()
+    request = product_action_request_factory('GET', config['CONNECT_JWT_SECRET'])
+    ext = IdeasPortalExtension(None, logger, config)
+    
+    result = ext.execute_product_action(request)
+    assert result.status == 'success'
+    assert result.http_status == 302
+    assert re.match(re.compile(config['DEFAULT_REDIRECT']), result.headers['Location'])
